d347f02c801107fcae732c78e8e73de2eebd6df9
TryToStopMe <cebanudaniel28@gmail.com>
2020-06-15 14:42:12 +0300

    passed lesson16 + added a photo from course

---
+++ js/script_lesson16.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    //let num = 10;
+    console.log(num);
+}
+
+showFirstMessage("Hello World!"); //вызов функции
+console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+    //Тут с этой переменной можно делать какието операции, после чего
+    //закончить функцию и показать её результат. 
+
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log("Hello");
+};
+
+logger();
+
+const calc = (a, b) => {
+    console.log('1');
+    return a + b;
+};
